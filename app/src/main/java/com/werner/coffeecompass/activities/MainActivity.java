021df29b0335c192af1b01217d82d42b757baaf8
austinwerner <r.austin.werner@gmail.com>
2020-02-19 20:52:29 -0600

    Initial Commit
    Create repo with blank main activity.

---
+++ app/src/main/java/com/werner/coffeecompass/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.werner.coffeecompass.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.werner.coffeecompass.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
